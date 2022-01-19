a888c8b2020626f45a6dc3c7d1dd97c6c157c1a6
Houssem Hammami <hammamihossem99@gmail.com>
2022-01-19 15:13:43 +0100

    fix jsx eslint error

--- components/eleves/layout/emptyStateEleves.js
+++ components/eleves/layout/emptyStateEleves.js
@@ -15,14 +15,14 @@ export function EmptyStateEleves() {
         >
           <path
             vectorEffect="non-scaling-stroke"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width={2}
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
             d="M18 9v3m0 0v3m0-3h3m-3 0h-3m-2-5a4 4 0 11-8 0 4 4 0 018 0zM3 20a6 6 0 0112 0v1H3v-1z"
           />
         </svg>
         <h3 className="mt-2 text-sm font-medium text-gray-900">
-          Pas d'étudiants
+          Pas d&apos;étudiants
         </h3>
         <p className="mt-1 text-sm text-gray-500">
           Commencez par créer un nouvel étudiant.
