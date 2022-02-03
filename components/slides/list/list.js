6b1ece48f32ffe549c2330e219255c7bd3fc33c2
Houssem Hammami <hammamihossem99@gmail.com>
2022-02-03 18:19:52 +0100

    add animation to the list componegdsg<sgd<

--- components/slides/list/list.js
+++ components/slides/list/list.js
@@ -4,10 +4,15 @@ import { Fragment, useState } from "react";
 import { Dialog, Transition } from "@headlessui/react";
 import { XIcon } from "@heroicons/react/outline";
 import { useList } from "./context";
+import { useEffect } from "react/cjs/react.development";
 
 function ListStudents() {
   const { students = [], open, setOpen } = useList();
-  console.log(students);
+  const [ready, setReady] = useState(false);
+
+  useEffect(() => {
+    setReady(true);
+  }, []);
   return (
     <Transition.Root show={open} as={Fragment}>
       <Dialog
@@ -42,9 +47,20 @@ function ListStudents() {
                 <div className="h-full flex flex-col py-6 bg-white shadow-xl overflow-y-scroll">
                   <div className="px-4 sm:px-6">
                     <div className="flex items-start justify-between">
-                      <Dialog.Title className="text-lg font-medium text-gray-900">
-                        Liste de tous les élève dans ce groupe:
-                      </Dialog.Title>
+                      <Transition
+                        appear={true}
+                        show={ready}
+                        as={Fragment}
+                        enter="transform delay-500 ease-out de duration-500 transition"
+                        enterFrom="-translate-y-4 opacity-0"
+                        enterTo="translate-y-0 opacity-100"
+                      >
+                        <div>
+                          <Dialog.Title className="text-lg font-medium text-gray-900">
+                            Liste de tous les élève dans ce groupe:
+                          </Dialog.Title>
+                        </div>
+                      </Transition>
                       <div className="ml-3 h-7 flex items-center">
                         <button
                           type="button"
@@ -59,45 +75,48 @@ function ListStudents() {
                   </div>
                   <div className="mt-6 relative flex-1 px-4 sm:px-6">
                     <div className="flow-root">
-                      <ul role="list" className="-mb-8">
-                        {students?.map((student, id) => (
-                          <li key={id}>
-                            <div className="relative pb-6">
-                              {id !== students.length - 1 ? (
-                                <span
-                                  className="absolute top-4 left-4 -ml-px h-full w-0.5 bg-gray-200"
-                                  aria-hidden="true"
-                                />
-                              ) : null}
-                              <div className="relative flex space-x-3">
-                                <div>
-                                  <span className="bg-gray-400 h-8 w-8 rounded-full flex items-center justify-center ring-8 ring-white">
-                                    <UserIcon
-                                      className="h-5 w-5 text-white"
-                                      aria-hidden="true"
-                                    />
-                                  </span>
-                                </div>
-                                <div className="min-w-0 flex-1 pt-1.5 flex justify-between space-x-4">
+                      <Transition
+                        appear={true}
+                        show={ready}
+                        as={Fragment}
+                        enter="transform delay-700 ease-out de duration-500 transition"
+                        enterFrom="translate-y-4 opacity-0"
+                        enterTo="translate-y-0 opacity-100"
+                      >
+                        <ul role="list" className="-mb-8">
+                          {students?.map((student, id) => (
+                            <li key={id}>
+                              <div className="relative pb-3">
+                                <div className="relative flex space-x-3">
                                   <div>
-                                    <p className="text-sm text-gray-500">
-                                      <span className="text-gray-400 text-xs">
-                                        {id + 1}.
-                                      </span>
-                                      {student.name_eleve}
-                                    </p>
-                                  </div>
-                                  <div className="text-right text-sm whitespace-nowrap text-gray-500">
-                                    <span className="inline-flex flex-shrink-0  items-center px-2.5 mr-2 py-0.5 rounded-full text-xs font-medium bg-indigo-100 text-indigo-800">
-                                      {student.pack.nom}
+                                    <span className="bg-gray-400 h-8 w-8 rounded-full flex items-center justify-center ring-8 ring-white">
+                                      <UserIcon
+                                        className="h-5 w-5 text-white"
+                                        aria-hidden="true"
+                                      />
                                     </span>
+                                  </div>
+                                  <div className="min-w-0 flex-1 pt-1.5 flex justify-between space-x-4">
+                                    <div>
+                                      <p className="text-sm text-gray-500">
+                                        <span className="text-gray-400 text-xs">
+                                          {id + 1}.
+                                        </span>
+                                        {student.name_eleve}
+                                      </p>
+                                    </div>
+                                    <div className="text-right text-sm whitespace-nowrap text-gray-500">
+                                      <span className="inline-flex flex-shrink-0  items-center px-2.5 mr-2 py-0.5 rounded-full text-xs font-medium bg-indigo-100 text-indigo-800">
+                                        {student.pack.nom}
+                                      </span>
+                                    </div>
                                   </div>
                                 </div>
                               </div>
-                            </div>
-                          </li>
-                        ))}
-                      </ul>
+                            </li>
+                          ))}
+                        </ul>
+                      </Transition>
                     </div>
                   </div>
                 </div>
