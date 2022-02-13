44a19efb82cbb328393e10e810b8d563b6f0a15d
Houssem Hammami <hammamihossem99@gmail.com>
2022-02-13 03:12:10 +0100

    fix color

--- components/groupes/layout/emptyGroupTable.js
+++ components/groupes/layout/emptyGroupTable.js
@@ -14,16 +14,19 @@ const EmptyGroupTable = () => {
       enterFrom="translate-y-4 opacity-0"
       enterTo="translate-y-0 opacity-100"
     >
-      <div className="rounded-md bg-red-200 p-4">
+      <div className="rounded-md bg-indigo-200 p-4">
         <div className="flex">
           <div className="flex-shrink-0">
-            <XCircleIcon className="h-5 w-5 text-red-400" aria-hidden="true" />
+            <XCircleIcon
+              className="h-5 w-5 text-indigo-400"
+              aria-hidden="true"
+            />
           </div>
           <div className="ml-3">
-            <h3 className="text-sm font-medium text-red-800">
+            <h3 className="text-sm font-medium text-indigo-800">
               Aucun groupe trouvé
             </h3>
-            <div className="mt-2 text-sm text-red-700">
+            <div className="mt-2 text-sm text-indigo-700">
               <p>
                 il n&apos;y a pas de groupes dans la base de données, vous
                 pouvez commencer à les ajouter maintenant !.
@@ -32,7 +35,7 @@ const EmptyGroupTable = () => {
             <div className="mt-4">
               <div className="-mx-2 -my-1.5 flex">
                 <Link href={"/groupes/ajouter"}>
-                  <a className="bg-red-300 px-2 py-1.5 rounded-md text-sm font-medium text-red-800 hover:bg-red-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-red-50 focus:ring-red-600">
+                  <a className="bg-indigo-300 px-2 py-1.5 rounded-md text-sm font-medium text-indigo-800 hover:bg-indigo-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-indigo-50 focus:ring-indigo-600">
                     Nouvel groupe
                   </a>
                 </Link>
