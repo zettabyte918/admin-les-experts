b794d61fa4fe26974d9fedbbf57004cbccfb1907
Hammami Houssem <hammamihossem99@gmail.com>
2022-08-03 15:13:49 +0100

    proreebar

--- pages/fichier/index.js
+++ pages/fichier/index.js
@@ -3,36 +3,16 @@ import { useEffect, useState } from "react";
 
 const bytesToMegaBytes = (bytes) => bytes / 1024 ** 2;
 
-const config = {
-  onUploadProgress: function (progressEvent) {
+const Fichier = () => {
+  const [selectedFile, setSelectedFile] = useState([]);
+  const [progress, setProgress] = useState(0);
+
+  const onUploadProgress = (progressEvent) => {
     var percentCompleted = Math.round(
       (progressEvent.loaded * 100) / progressEvent.total
     );
-    console.log(percentCompleted);
-  },
-};
-
-const Fichier = () => {
-  const [selectedFile, setSelectedFile] = useState([]);
-
-  // const uploadFile = async () => {
-  //   const Files = new FormData();
-
-  //   Array.from(selectedFile).map((file) => Files.append("files", file));
-
-  //   axios
-  //     .post(
-  //       `${process.env.NEXT_PUBLIC_STRAPI_BACKEND_API_URL}/upload/`,
-  //       Files,
-  //       config
-  //     )
-  //     .then((response) => {
-  //       response.data.map((res) => alert(JSON.stringify(res)));
-  //     })
-  //     .catch((error) => {
-  //       alert(`${JSON.stringify(error.response.data)}`);
-  //     });
-  // };
+    setProgress(percentCompleted);
+  };
 
   const uploadFile = async () => {
     const formData = new FormData();
@@ -53,7 +33,7 @@ const Fichier = () => {
       .post(
         `${process.env.NEXT_PUBLIC_STRAPI_BACKEND_API_URL}/attachments/`,
         formData,
-        config
+        { onUploadProgress }
       )
       .then((response) => {
         console.log(response);
@@ -71,43 +51,15 @@ const Fichier = () => {
         name="files"
         multiple
       />
-      <div className="mt-1 sm:mt-0 sm:col-span-2">
-        <div className="max-w-lg flex justify-center bg-white px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">
-          <div className="space-y-1 text-center">
-            <svg
-              className="mx-auto h-12 w-12 text-gray-400"
-              stroke="currentColor"
-              fill="none"
-              viewBox="0 0 48 48"
-              aria-hidden="true"
-            >
-              <path
-                d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
-                strokeWidth={2}
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-            </svg>
-            <div className="flex text-sm text-gray-600">
-              <label
-                htmlFor="file-upload"
-                className="relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500"
-              >
-                <span>Upload a file</span>
-                <input
-                  id="file-upload"
-                  name="file-upload"
-                  type="file"
-                  className="sr-only"
-                />
-              </label>
-              <p className="pl-1">or drag and drop</p>
-            </div>
-            <p className="text-xs text-gray-500">PNG, JPG, GIF up to 10MB</p>
-          </div>
+
+      <div className="w-full bg-gray-200 rounded-full">
+        <div
+          style={{ width: `${progress}%` }}
+          className=" transition-[width] duration-1000 ease-in-out bg-blue-600 transform translate text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-l-full"
+        >
+          {progress}%
         </div>
       </div>
-
       <input
         type="button"
         onClick={uploadFile}
@@ -120,3 +72,22 @@ const Fichier = () => {
 
 Fichier.layout = "GlobalLayout";
 export default Fichier;
+
+// const uploadFile = async () => {
+//   const Files = new FormData();
+
+//   Array.from(selectedFile).map((file) => Files.append("files", file));
+
+//   axios
+//     .post(
+//       `${process.env.NEXT_PUBLIC_STRAPI_BACKEND_API_URL}/upload/`,
+//       Files,
+//       config
+//     )
+//     .then((response) => {
+//       response.data.map((res) => alert(JSON.stringify(res)));
+//     })
+//     .catch((error) => {
+//       alert(`${JSON.stringify(error.response.data)}`);
+//     });
+// };
